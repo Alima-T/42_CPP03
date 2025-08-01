f5136f35ba4314fbdd6fdbe720f92bb688e1b458
Alima-T <okhapka.a@gmail.com>
2025-08-01 23:32:07 +0200

    add constructors in the headder and implementation for attack

--- ex00/include/ClapTrap.hpp
+++ ex00/include/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 21:06:49 by aokhapki          #+#    #+#             */
-/*   Updated: 2025/07/31 23:15:09 by aokhapki         ###   ########.fr       */
+/*   Updated: 2025/08/01 22:02:54 by aokhapki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,16 @@ class ClapTrap
 private:
 	std::string m_name;
 	int m_hitPoints;
-	int energYPoints;
-	int m_atackDamage;
-	
-	
-	
+	int m_energyPoints;
+	int m_attackDamage;
 
 public:
+	ClapTrap();
+	ClapTrap(const std::string &name); // Constructor with name parameter
+	ClapTrap(const ClapTrap &src); // Copy constructor src = other object being copied
+	ClapTrap &operator+(const ClapTrap &rhs); // Copy assignment operator rhs = object being assigned
+	~ClapTrap();
+	
 	void attack(const std::string& target);
 	void takeDamage(unsigned int amount);
 	void beRepaired(unsigned int amount);
--- ex00/src/ClapTrap.cpp
+++ ex00/src/ClapTrap.cpp
@@ -6,9 +6,26 @@
 /*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 23:27:17 by aokhapki          #+#    #+#             */
-/*   Updated: 2025/07/31 23:28:58 by aokhapki         ###   ########.fr       */
+/*   Updated: 2025/08/01 23:31:23 by aokhapki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ClapTrap.hpp"
 
+ClapTrap::ClapTrap() : m_name("Default"), m_hitPoints(10), m_energyPoints(10), m_attackDamage(0) {}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if(m_hitPoints > 0 || m_energyPoints >0)
+	{
+		m_energyPoints--;
+		m_attackDamage++; //Not sure! Increment attack damage with each attack
+		std::cout << "ClapTrap " << m_name << " attacks " << target << ", causing " 
+		<< m_attackDamage <<  " points of damage!" << std::endl;
+	}
+	else
+	{
+		std::cout << "ClapTrap " << m_name << " can't attack without energy or hitpoints!" << std::endl;
+		return;
+	}
+}
