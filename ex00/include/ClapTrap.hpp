d126a851123c0966f9db632b64cd646088c0c6eb
Alima-T <okhapka.a@gmail.com>
2025-08-04 23:04:27 +0200

    add implementation of ClapTrap class ethods and constructors, destructors, operator= overloading

--- ex00/include/ClapTrap.hpp
+++ ex00/include/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 21:06:49 by aokhapki          #+#    #+#             */
-/*   Updated: 2025/08/01 22:02:54 by aokhapki         ###   ########.fr       */
+/*   Updated: 2025/08/04 23:04:02 by aokhapki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 #define CLAPTRAP_HPP
 #include <iostream>
 
+
 class ClapTrap
 {
 private:
 	std::string m_name;
-	int m_hitPoints;
-	int m_energyPoints;
-	int m_attackDamage;
+	int m_hitPoints; // health points, очки здоровья
+	int m_energyPoints; // очки энергии, энергия для атаки
+	int m_attackDamage; //урон от атаки
 
 public:
 	ClapTrap();
@@ -29,10 +30,30 @@ public:
 	ClapTrap &operator+(const ClapTrap &rhs); // Copy assignment operator rhs = object being assigned
 	~ClapTrap();
 	
-	void attack(const std::string& target);
-	void takeDamage(unsigned int amount);
-	void beRepaired(unsigned int amount);
-	
-	
+	void attack(const std::string& target); // Target is the name of the object being attacked
+	void takeDamage(unsigned int amount); // Amount is the damage taken, unsigned int to ensure non-negative values
+	void beRepaired(unsigned int amount); // количество восстановленных очков здоровья, Amount is the healing amount
 };
 #endif
+
+/*
+	Health: Hit points are essentially a measure of how much "health" a character has.
+	HP - health - a measure of how much "health" a character has.
+	способность персонажа или объекта противостоять повреждениям или усталости
+
+	Behavior:
+	attack() → if hit points > 0 and energy > 0:
+
+	prints:
+	ClapTrap <name> attacks <target>, causing <damage> points of damage!
+	subtracts 1 energy point.
+
+	takeDamage():
+	subtracts amount from hit points and prints what happened:
+	ClapTrap <name> takes <amount> points of damage!
+	
+	beRepaired() → if hit points > 0 and energy > 0:
+	prints:
+	ClapTrap <name> repairs itself, regaining <amount> hit points!
+	adds to hit points, subtracts 1 energy point.
+*/
--- ex00/src/ClapTrap.cpp
+++ ex00/src/ClapTrap.cpp
@@ -6,26 +6,86 @@
 /*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 23:27:17 by aokhapki          #+#    #+#             */
-/*   Updated: 2025/08/01 23:31:23 by aokhapki         ###   ########.fr       */
+/*   Updated: 2025/08/04 22:59:03 by aokhapki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ClapTrap.hpp"
 
-ClapTrap::ClapTrap() : m_name("Default"), m_hitPoints(10), m_energyPoints(10), m_attackDamage(0) {}
+ClapTrap::ClapTrap() : m_name("Default"), m_hitPoints(10), m_energyPoints(10), m_attackDamage(0) 
+{
+	std::cout << "ClapTrap default constructor called for " << m_name << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string& name) : m_name(name), m_hitPoints(10), m_energyPoints(10), m_attackDamage(0) 
+{
+	std::cout << "ClapTrap constructor called for " << m_name << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& other) : m_name(other.m_name), m_hitPoints(other.m_hitPoints), m_energyPoints(other.m_energyPoints), m_attackDamage(other.m_attackDamage) 
+{
+	std::cout << "ClapTrap copy constructor called for " << m_name << std::endl;
+}
+
+ClapTrap& ClapTrap::operator+(const ClapTrap& rhs) 
+{
+	if (this != &rhs) // Check for self-assignment
+	{
+		m_name = rhs.m_name;
+		m_hitPoints = rhs.m_hitPoints;
+		m_energyPoints = rhs.m_energyPoints;
+		m_attackDamage = rhs.m_attackDamage;
+	}
+	return *this;
+}
+ClapTrap::~ClapTrap() 
+{
+	std::cout << "ClapTrap destructor called for " << m_name << std::endl;
+}
 
 void ClapTrap::attack(const std::string& target)
 {
-	if(m_hitPoints > 0 || m_energyPoints >0)
+	if(m_hitPoints > 0 || m_energyPoints > 0)
 	{
 		m_energyPoints--;
-		m_attackDamage++; //Not sure! Increment attack damage with each attack
 		std::cout << "ClapTrap " << m_name << " attacks " << target << ", causing " 
-		<< m_attackDamage <<  " points of damage!" << std::endl;
+				  << m_attackDamage <<  " points of damage!" << std::endl;
 	}
 	else
 	{
-		std::cout << "ClapTrap " << m_name << " can't attack without energy or hitpoints!" << std::endl;
+		std::cout << "ClapTrap " << m_name << " can't attack without energy or hitpoints!" 
+				  << "Energy: " << m_energyPoints << ", Hit Points: " << m_hitPoints << std::endl;
 		return;
+	}
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if(m_hitPoints > 0)
+	{
+		// condition ? value_if_true : value_if_false, ensure we don't go below zero
+		unsigned int damage = (amount > m_hitPoints) ? m_hitPoints : amount; 
+		m_hitPoints -= damage;
+		std::cout << "ClapTrap " << m_name << " takes " << damage << " points of damage! Remaining hit points: " 
+				  << m_hitPoints << std::endl;
+	}
+	else
+	{
+		std::cout << "ClapTrap " << m_name << " is destroyed and has no hit points to take damage!" << std::endl;
+		return;
+	}
+}
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if(m_energyPoints > 0 || m_hitPoints > 0)
+	{
+		m_energyPoints--;
+		m_hitPoints += amount;
+		std::cout << "ClapTrap " << m_name << " repairs itself for " << amount 
+				  << " hit points! Current hit points: " << m_hitPoints << std::endl;
+	}
+	else
+	{
+		std::cout << "ClapTrap " << m_name << " has no energy left to repair!" << std::endl;
 	}
 }
