bc84241e8b66a275b31d9df3bf55fb6fb84c8d06
Alimat Okhapkina <aokhapki@1-B-2.42heilbronn.de>
2025-08-05 20:50:30 +0200

    add in ex01 ClapTrap.cpp and main

---
+++ ex01/src/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 18:38:57 by aokhapki          #+#    #+#             */
+/*   Updated: 2025/08/05 18:39:00 by aokhapki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/ClapTrap.hpp"
+
+
+int main()
+{
+	//TODO: test constructors, copy constructor, assignment operator, destructor
+	ClapTrap default1; // Default constructor
+	default1.attack("Test1");
+	default1.takeDamage(3);
+	default1.beRepaired(2);
+	ClapTrap robot ("Robot");// Constructor with name parameter
+	robot.attack("Kiborg");
+	robot.takeDamage(5);
+	robot.beRepaired(3);
+	robot.attack("Zelebra");
+	robot.takeDamage(10);
+	ClapTrap copyRobot1(robot); // Copy constructor
+	ClapTrap copyRobot2 = robot; // Copy initialization
+	ClapTrap assignedRobot;
+	assignedRobot = robot; // Assignment operator
+	return 0;
+}
