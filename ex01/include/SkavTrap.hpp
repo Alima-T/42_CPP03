a32960d464a0d9c02cad377b42f2adeccfe6513d
Alima-T <okhapka.a@gmail.com>
2025-08-06 16:50:19 +0200

    add a new file SkavTrap.hpp for child class ScavTrap

---
+++ ex01/include/SkavTrap.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   SkavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/06 16:42:30 by aokhapki          #+#    #+#             */
+/*   Updated: 2025/08/06 16:42:33 by aokhapki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"  // Наследуем всё из ClapTrap
+
+class ScavTrap : public ClapTrap {
+public:
+    ScavTrap();                              // Конструктор по умолчанию
+    ScavTrap(const std::string& name);       // Конструктор с именем
+    ScavTrap(const ScavTrap& other);         // Конструктор копирования
+    ScavTrap& operator=(const ScavTrap& other); // Оператор присваивания
+    ~ScavTrap();                             // Деструктор
+
+    void attack(const std::string& target);  // Переопределённая атака
+    void guardGate();                        // Специальный режим охраны
+};
+
+#endif
