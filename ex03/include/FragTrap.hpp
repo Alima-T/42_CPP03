9a6e5d25425b2e8abb081ffad1ac845b2f8b70c2
Alima-T <okhapka.a@gmail.com>
2025-08-07 14:13:20 +0200

    add Makefile and headders of parents

---
+++ ex03/include/FragTrap.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/07 13:16:17 by aokhapki          #+#    #+#             */
+/*   Updated: 2025/08/07 13:42:51 by aokhapki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap {
+public:
+    FragTrap();
+    FragTrap(const std::string& name);
+    FragTrap(const FragTrap& other);
+    FragTrap& operator=(const FragTrap& other);
+    ~FragTrap();
+
+    void highFivesGuys(void);
+};
+
+#endif
